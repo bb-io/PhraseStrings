80e27e80c1ea388cca76e72ffdfab72340f80da4
RiabushenkoA <ariabushenko@blackbird.io>
2025-06-09 18:22:46 +0300

    Added webhook 'On job competed'

---
+++ Apps.PhraseStrings/Webhooks/Models/WebhookListResponse.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace Apps.PhraseStrings.Webhooks.Models
+{
+    public class WebhookListResponse
+    {
+        [JsonProperty("webhooks")]
+        public List<WebhookResponseItem> Webhooks { get; set; }
+    }
+
+    public class WebhookResponseItem
+    {
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("callback_url")]
+        public string? CallbackUrl { get; set; }
+
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+
+        [JsonProperty("events")]
+        public string? Events { get; set; }
+
+        [JsonProperty("active")]
+        public bool? Active { get; set; }
+
+        [JsonProperty("include_branches")]
+        public bool? IncludeBranches { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Apps.PhraseStrings/Webhooks/WebhookList.cs
@@ -0,0 +1,39 @@
+﻿using Apps.PhraseStrings.Model.Project;
+using Apps.PhraseStrings.Webhooks.Handler;
+using Apps.PhraseStrings.Webhooks.Models;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Apps.PhraseStrings.Webhooks
+{
+    [WebhookList]
+    public class WebhookList
+    {
+        [Webhook("On job completed", typeof(JobCompletedHandler), Description = "Triggers when job completed")]
+        public Task<WebhookResponse<JobCompleteWebhookResponse>> OnJobCompleted(WebhookRequest webhookRequest, [WebhookParameter(true)] ProjectRequest project)
+        {
+            var root = GetPayload<JobCompleteWebhookResponse>(webhookRequest);
+
+            return Task.FromResult(new WebhookResponse<JobCompleteWebhookResponse>()
+            {
+                Result = root
+            });
+        }
+
+
+        private WebhookResponse<T> GetPreflightResponse<T>() where T : class => new()
+        {
+            HttpResponseMessage = new HttpResponseMessage(HttpStatusCode.OK),
+            ReceivedWebhookRequestType = WebhookRequestType.Preflight
+        };
+
+        private T GetPayload<T>(WebhookRequest webhookRequest) where T : class
+        {
+            var payload = webhookRequest.Body.ToString();
+            ArgumentException.ThrowIfNullOrEmpty(payload, nameof(webhookRequest.Body));
+
+            return JsonConvert.DeserializeObject<T>(payload)!;
+        }
+    }
+}
