dd139b01138c9119f39ff116eff8cac788ce7cdd
ce-nistal <cecilia.nistal@blackbird.io>
2025-08-13 16:17:52 -0300

    Removed Beta tag

--- Apps.PhraseStrings/Actions/FigmaActions.cs
+++ Apps.PhraseStrings/Actions/FigmaActions.cs
@@ -4,7 +4,6 @@ using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
-using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using RestSharp;
 
 namespace Apps.PhraseStrings.Actions
