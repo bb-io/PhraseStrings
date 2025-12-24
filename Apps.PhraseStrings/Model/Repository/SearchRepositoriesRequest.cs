785f5dca1c7a924e74766650e42a4f2a148f6042
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-12-24 18:08:40 +0200

    add IgnoreInactiveRepos filter to 'Search repositories'

---
+++ Apps.PhraseStrings/Model/Repository/SearchRepositoriesRequest.cs
@@ -0,0 +1,10 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.PhraseStrings.Model.Repository;
+
+public class SearchRepositoriesRequest
+{
+    [Display("Ignore inactive repository syncs", 
+        Description = "Do not include inactive repository syncs in the response. False by default")]
+    public bool? IgnoreInactiveRepos { get; set; }
+}
